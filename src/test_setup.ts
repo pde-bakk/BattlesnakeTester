78e6d2ec78a394687bcfafce0c8515116b4c5957
pde-bakk <pde-bakk@student.codam.nl>
2025-10-28 20:26:31 +0100

    boom

--- src/test_setup.ts
+++ src/test_setup.ts
@@ -1,14 +1,18 @@
 import chai from 'chai';
-import chaiHttp from 'chai-http';
+import * as chaiHttp from 'chai-http';
 import { checkForGoodResponse, getUrl } from './test_helper';
 
 chai.use(chaiHttp);
+console.log('Has chai.request:', typeof (chai as any).request);
+
 export const expect = chai.expect;
 
-// Extend Chai with request method for proper typing
-export const chaiWithHttp = chai as typeof chai & {
-    request: (url: string) => any;
-};
+export const chaiWithHttp = chai;
+
+// // Extend Chai with request method for proper typing
+// export const chaiWithHttp = chai as typeof chai & {
+//     request: (url: string) => any;
+// };
 
 export const url: string = getUrl();
 
